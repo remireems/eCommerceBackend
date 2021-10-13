b3ed8e7c47a085ae9d2be8da55fb2141661852db
Remi <75871744+remireems@users.noreply.github.com>
2021-10-12 19:11:24 -0700

    added requirements for model

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,50 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize')
+const { Model, DataTypes, DECIMAL, NUMBER } = require('sequelize')
 // import our database connection from config.js
 const sequelize = require('../config/connection')
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      isDecimal: true,
+      // validate: {
+      //   isDecimal: true,
+      //   DECIMAL: true
+      // }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true,
+      // validate: {
+      //   isNumeric: true,
+      //   NUMBER: true
+      // }
+    },
+    category_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
