2b959d003fdb86b06ab87b7286779316562bed40
Remi <75871744+remireems@users.noreply.github.com>
2021-10-12 18:38:20 -0700

    added code

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 // import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express()
 
@@ -9,4 +11,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+sequelize.sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.log(err))
