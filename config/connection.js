b9ad6b5cf2ba2e32ec0a1194723f65431f9e7f2f
Remi <75871744+remireems@users.noreply.github.com>
2021-10-13 20:11:58 -0700

    fixed code and reinstalled packages

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:rootroot@localhost:3306/ecommerce_db`)
+const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.SECRET}@localhost:3306/ecommerce_db`)
 
 module.exports = sequelize
