cfd24b8e910855d41507c57edc33efff6212a98d
Remi <75871744+remireems@users.noreply.github.com>
2021-10-13 18:45:48 -0700

    changed variable names

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -22,14 +22,14 @@ router.get('/categories/:id', (req, res) => {
 router.post('/categories', (req, res) => {
   // create a new category
   Category.create(req.body)
-    .then(category => res.json(category))
+    .then(newCat => res.json(newCat))
     .catch(err => console.log(err))
 })
 
 router.put('/categories/:id', (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, { where: { id: req.params.id } })
-    .then(category => res.json(category))
+    .then(upCat => res.json(upCat))
     .catch(err => console.log(err))
 })
 
